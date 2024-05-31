06a7ad6efca2c00760ad87912dfc298ccdc5fc41
EnticG1 <nael.sing@gmail.com>
2024-05-31 18:15:35 +0700

    Added GET, UPDATE, POST, and DELETE for user data

---
+++ server/controllers/userController.js
@@ -0,0 +1,85 @@
+const User = require("../models/userModel")
+const mongoose = require('mongoose')
+
+// GET all user
+const getUsers = async (req, res) => {
+    const users = await User.find({}).sort({createdAt: -1})
+
+    res.status(200).json(users)
+}
+
+// GET a single user
+const getUser = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'User not found'})
+    }
+
+    const user = await User.findById(id)
+
+    if(!user){
+        return res.status(404).json({error: 'User not found'})
+    }
+
+    res.status(200).json(user)
+}
+
+// Create new user
+const createUser = async (req, res) => {
+    const {name, description, email, password} = req.body
+
+    // add doc to db
+    try{
+        const user = await User.create({name, description, email, password})
+        res.status(200).json(user)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+// Delete a user
+const deleteUser = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'User not found'})
+    }
+
+    const user = await User.findOneAndDelete({_id: id})
+
+    if(!user){
+        return res.status(400).json({error: 'User not found'})
+    }
+
+    res.status(200).json(user)
+}
+
+// Update a user
+
+const updateUser = async (req, res) =>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'User not found'})
+    }
+
+    const user = await User.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if(!user){
+        return res.status(400).json({error: 'User not found'})
+    }
+
+    res.status(200).json(user)
+}
+
+// Export function
+module.exports = {
+    getUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,38 +1,27 @@
 const express = require('express')
-const User = require("../models/userModel")
+const {
+    createUser,
+    getUsers,
+    getUser,
+    deleteUser,
+    updateUser
+} = require("../controllers/userController")
 
 const router = express.Router()
 
 // GET all user data
-router.get('/', (req, res) => {
-    res.json({msg: 'GET all user data'})
-})
+router.get('/', getUsers)
 
 //GET a single user data
-router.get('/:id', (req, res) => {
-    res.json({msg: 'GET 1 user data'})
-})
+router.get('/:id', getUser)
 
 //POST new user data
-router.post('/', async (req, res) => {
-    const {name, description, email, password} = req.body
+router.post('/', createUser)
 
-    try{
-        const user = await User.create({name, description, email, password})
-        res.status(200).json(user)
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-})
-
-//DLETE user data
-router.post('/:id', (req, res) => {
-    res.json({msg: 'DELETE a user data'})
-})
+//DELETE user data
+router.delete('/:id', deleteUser)
 
 //UPDATE a user data
-router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a user data'})
-})
+router.patch('/:id', updateUser)
 
 module.exports = router
